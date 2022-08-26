c5f85571bcb9d23697f8e300564c01a453e5a267
MagnusThiemer <thiemer.jensen@gmail.com>
2022-08-26 10:15:00 +0200

    add fixes to nav and gallery

--- src/components/GalleryContainer.js
+++ src/components/GalleryContainer.js
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion'
 import { useState } from 'react'
+import CornerTriangle from './CornerTriangle';
 import Lightbox from './Lightbox';
 
 
@@ -17,13 +18,51 @@ const GalleryContainer = ({data}) => {
       }
     },
   }
+  const hoverVariants = {
+    hidden: {
+      opacity: 0,
+      x: 0
+    },
+    hover: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.5,
+        delayChildren: 0.3
+      }
+    }
+  }
+  const children = {
+    hidden: {
+      opacity: 0,
+      x: 0
+    },
+    hover: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.1,
+        ease: 'easeIn'
+      }
+    }
+  }
+
+
   const [lightboxSrc, setLightboxSrc] = useState();
   const [lightboxAlt, setLightboxAlt] = useState()
+  const [hover, setHover] = useState();
+
+  const handleToggle = (e) => {
+    if(e.target.nextElementSibling){
+      e.target.nextElementSibling.classList.toggle('opacity-1')
+      e.target.nextElementSibling.classList.toggle('opacity-0')
+    }
+  }
 
   const [lightboxOn, setLightboxOn] = useState(false)
   const handleClick = (event) => {
-    setLightboxSrc(event.target.src);
-    setLightboxAlt(event.target.alt);
+    setLightboxSrc(event.target.parentNode.parentNode.querySelector('img').src);
+    setLightboxAlt(event.target.parentNode.parentNode.querySelector('img').alt);
     setLightboxOn(true)
   }
 
@@ -32,7 +71,15 @@ const GalleryContainer = ({data}) => {
       {lightboxOn && <Lightbox src={lightboxSrc} alt={lightboxAlt} setLightboxOn={setLightboxOn}/>}
       <motion.div variants={variants} initial='hidden' whileInView='visible' className='overflow-x-hidden flex flex-row flex-wrap justify-center'>
         {data && data.map((image, index) => (
-          <img src={image.asset.url} alt={image.description} key={index} className='max-h-[200px]' onClick={(event) => handleClick(event)}/>
+          <div key={index} className='grid grid-rows-1 grid-cols-1 ' onClick={(event) => handleClick(event)}>
+            <img src={image.asset.url} alt={image.description}  className='max-h-[200px] col-start-1 row-start-1' onClick={(event) => handleClick(event)}/>
+            <motion.div variants={hoverVariants} whileHover='hover' initial='hidden' className="col-start-1 row-start-1 bg-black bg-opacity-50" >
+              <motion.div variants={children} whileHover='hover' initial='hidden' className="relative h-full w-full">
+                <CornerTriangle position='top-left'/>
+                <CornerTriangle position='bottom-right'/>
+              </motion.div>
+            </motion.div>
+          </div>
         ))}
       </motion.div>
       
--- src/templates/Nav.js
+++ src/templates/Nav.js
@@ -2,9 +2,19 @@ import NavItem from "../components/NavItem";
 import logo from "../assets/icon/Logo_main.svg"
 import { useState } from "react";
 import CornerTriangle from "../components/CornerTriangle";
+import { motion } from 'framer-motion'
 
 const Nav = () => {
   const [toggle, setToggle] = useState(false)
+
+  const variants = {
+    visible: {
+      transition: {
+        ease: 'easeInOut',
+        duration: 500
+      }
+    }
+  }
 
   return ( 
     <header className="w-full py-6 bg-black border-y border-primary overflow-hidden sticky top-0 z-20">
@@ -13,11 +23,11 @@ const Nav = () => {
       <nav className="flex justify-between items-center w-4/5 mx-auto flex-wrap">
         <img src={logo} alt="" className=""/>
           <div className="h-7 w-10 md:hidden flex flex-col justify-between" onClick={() => setToggle(!toggle)}>
-            <div className={`w-full h-[2px] bg-white rounded-sm duration-400 ${toggle && 'rotate-45 translate-y-2'}`}></div>
-            <div className={`w-full h-[2px] bg-white rounded-sm duration-400 ${toggle && 'opacity-0'}`}></div>
-            <div className={`w-full h-[2px] bg-white rounded-sm duration-400 ${toggle && '-rotate-45 -translate-y-2'}`}></div>
+            <motion.div variants={variants} initial='hidden' animate='visible' className={`w-full h-[2px] bg-white rounded-sm duration-400 origin-center ${toggle && 'rotate-45 translate-y-2 origin-center'}`}></motion.div>
+            <motion.div variants={variants} initial='hidden' animate='visible' className={`w-full h-[2px] bg-white rounded-sm duration-400 ${toggle && 'opacity-0'}`}></motion.div>
+            <motion.div variants={variants} initial='hidden' animate='visible' className={`w-full h-[2px] bg-white rounded-sm duration-400 origin-center ${toggle && '-rotate-45 -translate-y-4'}`}></motion.div>
           </div>
-          <ul className={`md:flex flex-col md:flex-row md:visible md:relative ${!toggle && `hidden`} ${toggle && 'flex w-full items-end'}`}>
+          <ul className={`md:flex flex-col md:flex-row md:visible md:relative bg-black ${!toggle && `hidden`} ${toggle && 'flex w-full items-end'}`}>
             <NavItem dest='/' text='Home' />
             <NavItem dest='/' text='About Us' />
             <NavItem dest='/blog' text='Blog' />
