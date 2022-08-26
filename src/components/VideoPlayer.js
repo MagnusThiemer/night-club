a5f06d9c35f01c8eae353220d94b0ec74c7b0c21
MagnusThiemer <thiemer.jensen@gmail.com>
2022-08-26 12:02:53 +0200

    videoplayer border fix

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -10,7 +10,7 @@ const VideoPlayer = () => {
 
   return ( 
     <>
-      <div className='relative max-w-3xl mx-auto'>
+      <div className='relative max-w-3xl mb-6 mx-auto border border-x-transparent border-y-primary'>
         <video id='video' className='md:max-w-3xl w-full mx-auto' controls>
           <source src={video} type="video/mp4" />
           Your browser does not support the video tag.
